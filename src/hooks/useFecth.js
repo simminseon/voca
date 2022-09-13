8fd0d86522705899909789ca74f4c741ee544d45
simminseon <alstjs7440@naver.com>
2022-09-13 16:45:38 +0900

    test

--- src/hooks/useFecth.js
+++ src/hooks/useFecth.js
@@ -4,13 +4,9 @@ export default function useFetch(url) {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch(url)
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setData(data);
-            });
+        fetch(url).then((data) => {
+            setData(data);
+        });
     }, [url]);
 
     return data;
