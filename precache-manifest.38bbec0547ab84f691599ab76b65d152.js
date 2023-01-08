78ccd1f2ee7fbccea13173c87d15c55c2ea50fc8
simminseon <38029685+simminseon@users.noreply.github.com>
2023-01-08 09:36:17 +0000

    Deploying to gh-pages from @ simminseon/voca@9cab150b89bf03bd896293473c52c5e51c0375fe 🚀

---
+++ precache-manifest.38bbec0547ab84f691599ab76b65d152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5f8513973ccdba4e1e1",
+    "url": "/voca/static/css/main.846ba80a.chunk.css"
+  },
+  {
+    "revision": "c5f8513973ccdba4e1e1",
+    "url": "/voca/static/js/main.c5f85139.chunk.js"
+  },
+  {
+    "revision": "27110643c10c397eef7d",
+    "url": "/voca/static/js/1.27110643.chunk.js"
+  },
+  {
+    "revision": "7418a8b3beb8d1b2ff74",
+    "url": "/voca/static/js/runtime~main.7418a8b3.js"
+  },
+  {
+    "revision": "04fd236741411e580236d4604ed6602e",
+    "url": "/voca/index.html"
+  }
+];
